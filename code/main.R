ce1d1287c1e2c8738fc164293909d1d0921d75fe
samuel-rosa <alessandrosamuelrosa@gmail.com>
2017-04-28 12:05:50 -0300

    processamento dos metadados

--- code/main.R
+++ code/main.R
@@ -1,2 +1,239 @@
 # Calcular contribuições e gerar metadados para website
 
+# Preparar ambiente de trabalho
+rm(list = ls())
+source("code/helper.R")
+
+# ctb0001 ####
+createSiteMetadata(
+  n = "ctb0001",
+  dataset =
+    "https://docs.google.com/spreadsheets/d/1FQpmLSeVsbVDeFEwBzLQO1j6_m_G5PZ58DRPr32fhAc/edit#gid=1085102806",
+  observations =
+    "https://docs.google.com/spreadsheets/d/1FQpmLSeVsbVDeFEwBzLQO1j6_m_G5PZ58DRPr32fhAc/edit#gid=133250549",
+  layer =
+    "https://docs.google.com/spreadsheets/d/1FQpmLSeVsbVDeFEwBzLQO1j6_m_G5PZ58DRPr32fhAc/edit#gid=72748886",
+  sharing = 
+    "https://docs.google.com/spreadsheets/d/1FQpmLSeVsbVDeFEwBzLQO1j6_m_G5PZ58DRPr32fhAc/edit?usp=sharing"
+)
+
+# ctb0002 ####
+createSiteMetadata(
+  n = "ctb0002",
+  dataset =
+    "https://docs.google.com/spreadsheets/d/1TSt1bM_JWo15sGkPWgoUvHJMCMjn2y_9LuDQo5M0MNE/edit#gid=1215698767",
+  observations =
+    "https://docs.google.com/spreadsheets/d/1TSt1bM_JWo15sGkPWgoUvHJMCMjn2y_9LuDQo5M0MNE/edit#gid=599711036",
+  layer =
+    "https://docs.google.com/spreadsheets/d/1TSt1bM_JWo15sGkPWgoUvHJMCMjn2y_9LuDQo5M0MNE/edit#gid=1143737782",
+  sharing =
+    "https://docs.google.com/spreadsheets/d/1FQpmLSeVsbVDeFEwBzLQO1j6_m_G5PZ58DRPr32fhAc/edit?usp=sharing"
+)
+
+# ctb0004 ####
+createSiteMetadata(
+  n = "ctb0004",
+  dataset =
+    "https://docs.google.com/spreadsheets/d/1nwz6yvoomItBnSea26h5sAD-2PCguZyzEH8oFMMFRkA/edit#gid=1085102806",
+  observations =
+    "https://docs.google.com/spreadsheets/d/1nwz6yvoomItBnSea26h5sAD-2PCguZyzEH8oFMMFRkA/edit#gid=133250549",
+  layer =
+    "https://docs.google.com/spreadsheets/d/1nwz6yvoomItBnSea26h5sAD-2PCguZyzEH8oFMMFRkA/edit#gid=72748886",
+  sharing = 
+    "https://docs.google.com/spreadsheets/d/1nwz6yvoomItBnSea26h5sAD-2PCguZyzEH8oFMMFRkA/edit?usp=sharing"
+)
+
+# ctb0005 ####
+createSiteMetadata(
+  n = "ctb0005",
+  dataset =
+    "https://docs.google.com/spreadsheets/d/1oPacCIchm-cGu-TSfQq8sBxsGC9xZx7urw5FSobS1mU/edit#gid=1085102806",
+  observations =
+    "https://docs.google.com/spreadsheets/d/1oPacCIchm-cGu-TSfQq8sBxsGC9xZx7urw5FSobS1mU/edit#gid=133250549",
+  layer =
+    "https://docs.google.com/spreadsheets/d/1oPacCIchm-cGu-TSfQq8sBxsGC9xZx7urw5FSobS1mU/edit#gid=72748886",
+  sharing = 
+    "https://docs.google.com/spreadsheets/d/1oPacCIchm-cGu-TSfQq8sBxsGC9xZx7urw5FSobS1mU/edit?usp=sharing"
+)
+
+ctb0006 ####
+createSiteMetadata(
+  n = "ctb0006",
+  dataset =
+    "https://docs.google.com/spreadsheets/d/1lG2_VgTtbhXRe8zY6A1nRakumFYU_69qu3AFW0Onqe0/edit#gid=1085102806",
+  observations =
+    "https://docs.google.com/spreadsheets/d/1lG2_VgTtbhXRe8zY6A1nRakumFYU_69qu3AFW0Onqe0/edit#gid=133250549",
+  layer =
+    "https://docs.google.com/spreadsheets/d/1lG2_VgTtbhXRe8zY6A1nRakumFYU_69qu3AFW0Onqe0/edit#gid=72748886",
+  sharing =
+    "https://docs.google.com/spreadsheets/d/1lG2_VgTtbhXRe8zY6A1nRakumFYU_69qu3AFW0Onqe0/edit?usp=sharing"
+)
+
+# ctb0007 ####
+createSiteMetadata(
+  n = "ctb0007",
+  dataset =
+    "https://docs.google.com/spreadsheets/d/1H-B8ReAc9BepqfQFKnGItXH_04DIlgJuk8ACFnvgZEA/edit#gid=1085102806",
+  observations =
+    "https://docs.google.com/spreadsheets/d/1H-B8ReAc9BepqfQFKnGItXH_04DIlgJuk8ACFnvgZEA/edit#gid=133250549",
+  layer =
+    "https://docs.google.com/spreadsheets/d/1H-B8ReAc9BepqfQFKnGItXH_04DIlgJuk8ACFnvgZEA/edit#gid=72748886",
+  sharing = 
+    "https://docs.google.com/spreadsheets/d/1H-B8ReAc9BepqfQFKnGItXH_04DIlgJuk8ACFnvgZEA/edit?usp=sharing"
+)
+
+# ctb0008 ####
+createSiteMetadata(
+  n = "ctb0008",
+  dataset =
+    "https://docs.google.com/spreadsheets/d/1Vk4NZkzjQ_xjponhrSBw8y1ue6-yXVpbOSzYTUt4S8Q/edit#gid=1085102806",
+  observations =
+    "https://docs.google.com/spreadsheets/d/1Vk4NZkzjQ_xjponhrSBw8y1ue6-yXVpbOSzYTUt4S8Q/edit#gid=133250549",
+  layer =
+    "https://docs.google.com/spreadsheets/d/1Vk4NZkzjQ_xjponhrSBw8y1ue6-yXVpbOSzYTUt4S8Q/edit#gid=72748886",
+  sharing = 
+    "https://docs.google.com/spreadsheets/d/1Vk4NZkzjQ_xjponhrSBw8y1ue6-yXVpbOSzYTUt4S8Q/edit?usp=sharing"
+)
+
+# ctb0009 ####
+createSiteMetadata(
+  n = "ctb0009",
+  dataset =
+    "https://docs.google.com/spreadsheets/d/1om1Ida04FcAegLCZJ9CQD9J_cMBW8Rti0HosgFxyb_A/edit#gid=1085102806",
+  observations =
+    "https://docs.google.com/spreadsheets/d/1om1Ida04FcAegLCZJ9CQD9J_cMBW8Rti0HosgFxyb_A/edit#gid=133250549",
+  layer =
+    "https://docs.google.com/spreadsheets/d/1om1Ida04FcAegLCZJ9CQD9J_cMBW8Rti0HosgFxyb_A/edit#gid=72748886",
+  sharing = 
+    "https://docs.google.com/spreadsheets/d/1om1Ida04FcAegLCZJ9CQD9J_cMBW8Rti0HosgFxyb_A/edit?usp=sharing"
+)
+
+# ctb0010 ####
+createSiteMetadata(
+  n = "ctb0010",
+  dataset =
+    "https://docs.google.com/spreadsheets/d/1dLyrP48NP380AlA1P_pV1d3sKCpeY72gg4WJtikaQ6Q/edit#gid=1085102806",
+  observations =
+    "https://docs.google.com/spreadsheets/d/1dLyrP48NP380AlA1P_pV1d3sKCpeY72gg4WJtikaQ6Q/edit#gid=133250549",
+  layer =
+    "https://docs.google.com/spreadsheets/d/1dLyrP48NP380AlA1P_pV1d3sKCpeY72gg4WJtikaQ6Q/edit#gid=72748886",
+  sharing = 
+    "https://docs.google.com/spreadsheets/d/1dLyrP48NP380AlA1P_pV1d3sKCpeY72gg4WJtikaQ6Q/edit?usp=sharing"
+)
+
+# ctb0011 ####
+createSiteMetadata(
+  n = "ctb0011",
+  dataset =
+    "https://docs.google.com/spreadsheets/d/1v1QPkS-hL6MStd7MaASasrJpNh5GIC1dFkPSc0nny5E/edit#gid=1085102806",
+  observations =
+    "https://docs.google.com/spreadsheets/d/1v1QPkS-hL6MStd7MaASasrJpNh5GIC1dFkPSc0nny5E/edit#gid=133250549",
+  layer =
+    "https://docs.google.com/spreadsheets/d/1v1QPkS-hL6MStd7MaASasrJpNh5GIC1dFkPSc0nny5E/edit#gid=72748886",
+  sharing = 
+    "https://docs.google.com/spreadsheets/d/1v1QPkS-hL6MStd7MaASasrJpNh5GIC1dFkPSc0nny5E/edit?usp=sharing"
+)
+
+# ctb0012 ####
+createSiteMetadata(
+  n = "ctb0012",
+  dataset =
+    "https://docs.google.com/spreadsheets/d/1ivDQsv409AEwdic7LTo0p-hlhtbFptswvVX2nQCcg8I/edit#gid=1085102806",
+  observations =
+    "https://docs.google.com/spreadsheets/d/1ivDQsv409AEwdic7LTo0p-hlhtbFptswvVX2nQCcg8I/edit#gid=133250549",
+  layer =
+    "https://docs.google.com/spreadsheets/d/1ivDQsv409AEwdic7LTo0p-hlhtbFptswvVX2nQCcg8I/edit#gid=72748886",
+  sharing = 
+    "https://docs.google.com/spreadsheets/d/1ivDQsv409AEwdic7LTo0p-hlhtbFptswvVX2nQCcg8I/edit?usp=sharing"
+)
+
+# ctb0013 ####
+createSiteMetadata(
+  n = "ctb0013",
+  dataset =
+    "https://docs.google.com/spreadsheets/d/1J7YyP73MM3k9GtYzMx9pG6U4Cyyh9rZ3z6CNNS_bg58/edit#gid=1085102806",
+  observations =
+    "https://docs.google.com/spreadsheets/d/1J7YyP73MM3k9GtYzMx9pG6U4Cyyh9rZ3z6CNNS_bg58/edit#gid=133250549",
+  layer =
+    "https://docs.google.com/spreadsheets/d/1J7YyP73MM3k9GtYzMx9pG6U4Cyyh9rZ3z6CNNS_bg58/edit#gid=72748886",
+  sharing = 
+    "https://docs.google.com/spreadsheets/d/1J7YyP73MM3k9GtYzMx9pG6U4Cyyh9rZ3z6CNNS_bg58/edit?usp=sharing"
+)
+
+# ctb0014 ####
+createSiteMetadata(
+  n = "ctb0014",
+  dataset =
+    "https://docs.google.com/spreadsheets/d/1uywOkShs1hhZMxi0E8XM2MqrSKl0z5K3vd3juMURF9M/edit#gid=1085102806",
+  observations =
+    "https://docs.google.com/spreadsheets/d/1uywOkShs1hhZMxi0E8XM2MqrSKl0z5K3vd3juMURF9M/edit#gid=133250549",
+  layer =
+    "https://docs.google.com/spreadsheets/d/1uywOkShs1hhZMxi0E8XM2MqrSKl0z5K3vd3juMURF9M/edit#gid=72748886",
+  sharing = 
+    "https://docs.google.com/spreadsheets/d/1uywOkShs1hhZMxi0E8XM2MqrSKl0z5K3vd3juMURF9M/edit?usp=sharing"
+)
+
+# ctb0015 ####
+createSiteMetadata(
+  n = "ctb0015",
+  dataset =
+    "https://docs.google.com/spreadsheets/d/1dRDH6khT78PXj31-a9EvTxq3Al1T3eWyckujQeP4BPM/edit#gid=1085102806",
+  observations =
+    "https://docs.google.com/spreadsheets/d/1dRDH6khT78PXj31-a9EvTxq3Al1T3eWyckujQeP4BPM/edit#gid=133250549",
+  layer =
+    "https://docs.google.com/spreadsheets/d/1dRDH6khT78PXj31-a9EvTxq3Al1T3eWyckujQeP4BPM/edit#gid=72748886",
+  sharing = 
+    "https://docs.google.com/spreadsheets/d/1dRDH6khT78PXj31-a9EvTxq3Al1T3eWyckujQeP4BPM/edit?usp=sharing"
+)
+
+# ctb0016 ####
+createSiteMetadata(
+  n = "ctb0016",
+  dataset =
+    "https://docs.google.com/spreadsheets/d/1yGGaV9hsUk-W_29XZs-Fp5OsxpuT1BndrSS3qUO3894/edit#gid=1085102806",
+  observations =
+    "https://docs.google.com/spreadsheets/d/1yGGaV9hsUk-W_29XZs-Fp5OsxpuT1BndrSS3qUO3894/edit#gid=133250549",
+  layer =
+    "https://docs.google.com/spreadsheets/d/1yGGaV9hsUk-W_29XZs-Fp5OsxpuT1BndrSS3qUO3894/edit#gid=72748886",
+  sharing = 
+    "https://docs.google.com/spreadsheets/d/1yGGaV9hsUk-W_29XZs-Fp5OsxpuT1BndrSS3qUO3894/edit?usp=sharing"
+)
+
+# ctb0022 ####
+createSiteMetadata(
+  n = "ctb0022",
+  dataset =
+    "https://docs.google.com/spreadsheets/d/1wQOGtvTxSPuoaqLYNXX1_nshSrxiTBNPoB8SaTopRR8/edit#gid=1085102806",
+  observations =
+    "https://docs.google.com/spreadsheets/d/1wQOGtvTxSPuoaqLYNXX1_nshSrxiTBNPoB8SaTopRR8/edit#gid=133250549",
+  layer =
+    "https://docs.google.com/spreadsheets/d/1wQOGtvTxSPuoaqLYNXX1_nshSrxiTBNPoB8SaTopRR8/edit#gid=72748886",
+  sharing = 
+    "https://docs.google.com/spreadsheets/d/1wQOGtvTxSPuoaqLYNXX1_nshSrxiTBNPoB8SaTopRR8/edit?usp=sharing"
+)
+
+# ctb0643 ####
+createSiteMetadata(
+  n = "ctb0643",
+  dataset =
+    "https://docs.google.com/spreadsheets/d/1qW7GUKs9WuGNMsBAzskaw1mxtGgOrbc04jcGiYv-xSU/edit#gid=1654671199",
+  observations =
+    "https://docs.google.com/spreadsheets/d/1fqsX4p8di42xCpRVuVRtQcCIjhCO6emZN2OojDpaKV0/edit#gid=423050149",
+  layer =
+    "https://docs.google.com/spreadsheets/d/1jO9tarQhP4TKHuR5pBeDXTm77NFYKbtjsuHU-jJ1jtc/edit#gid=756782505",
+  sharing = 
+    "https://drive.google.com/drive/folders/0B7xsLbrOA23oYmxNd2NaWkFGNUk?usp=sharing"
+)
+
+# ctb0672 ####
+createSiteMetadata(
+  n = "ctb0672",
+  dataset = 
+    "https://docs.google.com/spreadsheets/d/1_lp9UHUnKi0Y6kSg_OEno-xh_BzGvqin4qBdFmpdZ8M/edit#gid=1691823338",
+  observations = 
+    "https://docs.google.com/spreadsheets/d/1HyAVwK1comUPvErDOOEq80KfDazNi1LNd1uimjLMEv4/edit#gid=112014124",
+  layer = 
+    "https://docs.google.com/spreadsheets/d/1ohV68-30TUF-zmUL0t_aPsar84E71UElRT5aEb2i6F8/edit#gid=193673171",
+  sharing = 
+    "https://drive.google.com/drive/folders/0B7xsLbrOA23oRnRvRmlrMlRyQ1E?usp=sharing"
+)
