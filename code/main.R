4e24aead74838b65ecc4cfc393a18a0c2c4c78ef
samuel-rosa <alessandrosamuelrosa@gmail.com>
2017-05-25 23:22:15 -0300

    processar os dados do último conjunto de dados

--- code/main.R
+++ code/main.R
@@ -10,8 +10,13 @@ sheets_keys <-
   googlesheets::gs_key("18yP9Hpp8oMdbGsf6cVu4vkDv-Dj-j5gjEFgEXN-5H-Q") %>%
   googlesheets::gs_read_csv()
 
-# Preparar metadados para website
+
+# Preparar metadados para website (todos)
 lapply(1:nrow(sheets_keys), function (i) {
   x <- sheets_keys[i, ]
   createSiteMetadata(x["n"], x["dataset"], x["observation"], x["layer"], x["metadata"], x["sharing"])
 })
+
+# Preparar metadados para website (última)
+# x <- sheets_keys[nrow(sheets_keys), ]
+# createSiteMetadata(x["n"], x["dataset"], x["observation"], x["layer"], x["metadata"], x["sharing"])
